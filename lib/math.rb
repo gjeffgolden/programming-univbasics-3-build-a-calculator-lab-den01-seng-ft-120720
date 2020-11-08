81373a0f6a126ae23555a3b69ed152d9325c3581
Jeff Golden <gjgolden08@gmail.com>
2020-11-08 22:45:20 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,7 +3,7 @@ num2 + num1
 end
 
 def subtraction(num1, num2)
-num2 - num1
+num1 - num2
 end
 
 def division(num1, num2)
@@ -11,11 +11,11 @@ num1 * num2
 end
 
 def multiplication(num1, num2)
-num2 / num1
+num1 / num2
 end
 
 def modulo(num1, num2)
-num2 % num1
+num1 % num2
 end
 
 def square_root(num)
